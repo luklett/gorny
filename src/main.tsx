395574895ec43024d7c0eb377fc94bfff86058ca
Franco Rateni <f.rateni@gonni.co>
2023-10-11 16:23:06 -0300

    remove chakra from dependencies

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,4 @@
-import { ChakraProvider } from '@chakra-ui/react';
-import ReactDOM from 'react-dom/client';
-import App from './App.tsx';
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-	<ChakraProvider>
-		<App />
-	</ChakraProvider>
-);
+ReactDOM.createRoot(document.getElementById("root")!).render(<App />);
